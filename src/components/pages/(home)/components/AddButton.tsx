a99618e41ab2e1ab7dd77ff11fb0d34381f86390
mizzzuno <mizz22@outlook.jp>
2025-10-02 21:04:06 +0900

    feature: add AddButton

---
+++ src/components/pages/(home)/components/AddButton.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+import Stack from '@mui/material/Stack';
+
+export default function AddButton() {
+  return (
+    <Stack direction="row" spacing={2}>
+      <Button variant="outlined" startIcon={<AddIcon />} onClick={() => {
+        // Add your onClick logic here
+        console.log('Add button clicked');
+      }}>
+        Add Data
+      </Button>
+    </Stack>
+  );
+}
--- src/components/pages/(home)/page.tsx
+++ src/components/pages/(home)/page.tsx
@@ -1,21 +1,23 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import AddButton from "@/components/pages/(home)/components/AddButton";
 
 const ListView = dynamic(
   () => import("@/components/pages/(home)/components/ListView"),
-  { ssr: false },
+  { ssr: false }
 );
 
 const BasicSelect = dynamic(
   () => import("@/components/pages/(home)/components/Selector"),
-  { ssr: false },
+  { ssr: false }
 );
 
 export default function HomePage() {
   return (
     <>
       <BasicSelect />
+      <AddButton />
       <ListView />
     </>
   );
